f048a05b345f8b49f42ac7c0b73d6d920793f680
Fagner Santos <uprogramador@gmail.com>
2022-07-09 18:42:28 -0300

    Exercicio de herança e polimorfismo

---
+++ Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Inheritance.Entities;
+using System.Globalization;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Entre com o número de funcionários: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Dados do #{i} trabalhador:");
+                Console.Write("Terceirizado (s/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Quantidade de horas: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Valor por hora: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 's')
+                {
+                    Console.Write("Despesa adicional: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAGAMENTOS:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
